5c75e8b26593f06f462d136482c92c320e1c0944
ivane98 <i.vardoshvili22@gmail.com>
2025-11-21 22:14:06 +0400

    featured

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,18 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">{{ $slot
-    }}</a>
+@props(['size' => 'base', 'tag'])
+
+@php
+    $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300";
+
+
+    if ($size === 'small') {
+        $classes .= " px-2 py-1 text-xs";
+    }
+
+    if ($size === 'base') {
+        $classes .= " px-5 py-1 text-sm";
+    }
+
+    
+@endphp
+
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
